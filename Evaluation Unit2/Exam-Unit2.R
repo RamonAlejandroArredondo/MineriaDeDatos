478a397f170867a517642f2733c21572cd31730b
RamonAlejandroArredondo <ramonalejandro3096@gmail.com>
2021-05-10 20:27:07 -0700

    ExamUnit2_part2

--- Evaluation Unit2/Exam-Unit2.R
+++ Evaluation Unit2/Exam-Unit2.R
@@ -29,3 +29,32 @@ Data3 <- Data2[Data2$Studio == "Buena Vista Studios" | Data2$Studio == "Fox"
 
 
 
+# First, the ggplot2 libraries are loaded, in order to make the diagram.
+
+library(ggplot2)
+
+
+# P, is our variable, we call the ggplot function, where we will put the following arguments, first of all we call our data, 
+#in this case it is called Data3, and then in the aes or Aesthetic function, on the X axis we will put the genre y on the Y 
+#axis the gross us, followed by each study having a different color and the size being defined by the Budget
+
+p <- ggplot(Data3, aes(x=Genre, y=Gross_US, 
+                       color=Studio, size=Budget))
+
+
+# Now to the variable P, we add the geom_jitter and the geom_boxplot, but we will give the boxplot a size of 0.2, we will 
+#put the color black and a transparency of 0.5 or alpha of 0.5.
+
+# We will add the labs, to put, the general title of the graph, sub-titles of the X axis and the Y axis and also change the 
+#name of the Budget scale
+
+# Followed by the titles, we add a theme, where the letter of the sub-titles are in BlueViolet color and both have a font 
+#size of 18, the general title of the graphic was adjusted in the middle and a font size was given of 22 and to finish the 
+#whole graphic the font was changed by Comic Sans MS.
+
+p + geom_jitter() + geom_boxplot(size=0.2, color = "Black", alpha=0.5) +
+  labs(title = "Domestic Gross % by Genre", x = "Genre", y = "Gross % US") + scale_size(name="Budget $M") +
+  theme(axis.title.x = element_text(color = "BlueViolet", size = 18),
+        axis.title.y = element_text(color = "BlueViolet", size = 18),
+        plot.title = element_text(hjust = 0.5, size = 22),
+        text = element_text(family = "Comic Sans MS"))
